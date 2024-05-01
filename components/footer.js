4a148dc851aff16980e5af2b025c47232ef3e332
Diego Whiskey <d.calcar@hotmail.com>
2024-05-01 12:54:01 -0500

    [MODIFIED] Next.js target="_blank" noopener

--- components/footer.js
+++ components/footer.js
@@ -2,8 +2,8 @@ import { Box } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
-    <Box align="center" opacity={0.4} fontSize="sm">      
-      <a href='https://d-caldascaridad.github.io/wd-p/' target='_blank'>&copy; {new Date().getFullYear()} { " " }De Caldas. All Rights Reserved.</a>
+    <Box align="center" opacity={0.4} fontSize="sm">
+      <a href='https://deCaldas.github.io/home/'>&copy; {new Date().getFullYear()} {" "}Diego Whiskey. All Rights Reserved.</a>
     </Box>
   )
 }
